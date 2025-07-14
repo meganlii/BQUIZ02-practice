80f06035114d14f02638c58bcc10238bd9378dad
meganli <lovecezanne@gmail.com>
2025-07-14 16:09:02 +0800

    練習乙級術科第二題

---
+++ front/forgot.php
@@ -0,0 +1,25 @@
+<fieldset>
+  <legend>忘記密碼</legend>
+  <div>請輸入信箱以查詢密碼<< /div>
+
+      <div>
+        <input type="text" name="email" id="email">
+      </div>
+
+      <div id="answer"></div>
+      <div>
+        <button onclick="search()">尋找</button>
+      </div>
+</fieldset>
+
+<script>
+  function search() {
+    $.get("./api/chk_email.php", {
+        email: $("#email").val()
+      },
+      (res) => {
+        $("#answer").html(res);
+      })
+
+  }
+</script>
